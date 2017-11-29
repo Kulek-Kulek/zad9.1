b2661ee3e27631e0db1be4f88893c098d19daf5e
Krzysztof Ługowski <k.lugowski@yahoo.com>
2017-11-29 10:39:06 +0100

    Add new files

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+
+var triangle1Area = getTriangleArea(10, 15)
+var triangle2Area = getTriangleArea(20, 10)
+var triangle3Area = getTriangleArea(4, 8)
+
+
+function getTriangleArea (a,h) {
+	if ((a <= 0) && (h <= 0)) 
+		
+		console.log("Nieprawidłowe dane")
+	
+
+	if ((a > 0) && (h > 0)) 
+		return(a*h/2)
+	
+}
+
+console.log(getTriangleArea(10,6))
+console.log(getTriangleArea(triangle1Area))
+console.log(getTriangleArea(triangle1Area))
+console.log(getTriangleArea(triangle1Area))
+
+
+
+
+
+
+
+
+
+
+
+
